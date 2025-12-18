0ac967cca1abb003b379526b0f8cf3e0a5815ddb
yokamly <collaboration.cjes@gmail.com>
2025-12-18 14:26:13 +0000

    Add . for Laravel (Dokploy)

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    server: {
-        host: true,
-        hmr: false,
-    },
-
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        emptyOutDir: true,
-    },
-
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: false,
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
         }),
         tailwindcss(),
     ],
